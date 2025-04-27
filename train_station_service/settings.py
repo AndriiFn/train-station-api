b462aba9e9787aab3441c948e58360be8fff8f92
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-04-27 20:26:10 +0200

    add debug toolbar to settings

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
     "station",
     "user",
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "train_station_service.urls"
@@ -114,6 +116,11 @@ USE_I18N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = "user.User"
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Static files (CSS, JavaScript, Images)
