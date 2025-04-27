58bb12f12d6754a685789190bbac192ff3efa3f6
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-04-27 20:27:41 +0200

    implement views

--- station/views.py
+++ station/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from station.models import (
+    Route,
+    Station,
+    Ticket,
+    Crew,
+    Order,
+    Journey,
+    Train,
+    TrainType
+)
+from station.serializers import (
+    RouteSerializer,
+    StationSerializer,
+    TicketSerializer,
+    CrewSerializer,
+    OrderSerializer,
+    JourneySerializer,
+    TrainSerializer,
+    TrainTypeSerializer
+)
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
