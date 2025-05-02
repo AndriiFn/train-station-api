8fc75e040f2d908c6f7786d0e5d2c06fcc8b1758
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-05-02 18:21:21 +0200

    add MEDIA_ROOT and MEDIA_URL

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -128,6 +128,10 @@ INTERNAL_IPS = [
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
