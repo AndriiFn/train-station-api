6e43c79583841582d67621f4c9fc6160773e839d
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-05-03 12:12:12 +0200

    add REST_FRAMEWORK and SIMPLE_JWT to settings

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "station",
     "user",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,17 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "station.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
