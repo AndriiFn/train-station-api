2601d41a7cca871339cf1dd53221000b051daf8b
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-05-03 11:01:46 +0200

    add user.urls

--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -23,5 +23,6 @@ from train_station_service import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/station/", include("station.urls", namespace="station")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
